da700e4716c4327ff38357d32280ef6c7e5eee79
TK Yoon <tkyoon81@gmail.com>
2018-11-28 06:30:35 +0900

    소셜계정 로그인 화면 구성중
    Signed-off-by: TK Yoon <tkyoon81@gmail.com>

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ const db = require('./db');
 const express = require('express');
 const session = require('express-session');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
 
 const socketio = require('socket.io'); // 추가
 
