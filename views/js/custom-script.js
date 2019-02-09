1142fff62e6e5e396220976a0852aab211dc2a5c
TK Yoon <tkyoon81@gmail.com>
2019-02-09 17:05:39 +0900

    필사 작성 완료
    Signed-off-by: TK Yoon <tkyoon81@gmail.com>

--- views/js/custom-script.js
+++ views/js/custom-script.js
@@ -9,3 +9,19 @@ NOTE:
 ------
 PLACE HERE YOUR OWN JS CODES AND IF NEEDED.
 WE WILL RELEASE FUTURE UPDATES SO IN ORDER TO NOT OVERWRITE YOUR CUSTOM SCRIPT IT'S BETTER LIKE THIS. */
+
+
+$.fn.selectRange = function(start, end) {
+    return this.each(function() {
+        if (this.setSelectionRange) {
+            this.focus();
+            this.setSelectionRange(start, end);
+        } else if (this.createTextRange) {
+            var range = this.createTextRange();
+            range.collapse(true);
+            range.moveEnd('character', end);
+            range.moveStart('character', start);
+            range.select();
+        }
+    });
+};
